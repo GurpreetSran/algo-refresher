a17e7476bec6c494c399bf89480ce0bcdc10aaec
Gurpreet Singh <gurpreet.singh@trinnylondon.com>
2023-10-09 09:41:44 +0100

    fizzBuzz and Chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,39 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// Optimise it and understand other examples
+
+function chunk(array, size) {
+  const result = [];
+
+  while (array.length > 0) {
+    if (array.length <= size) {
+      result.push(array);
+      return result;
+    } else {
+      result.push(array.splice(0, size));
+    }
+  }
+
+  return result;
+}
+
+// function chunk(array, size) {
+//   const chunked = [];
+
+//   for (let element of array) {
+//     debugger;
+
+//     const last = chunked[chunked.length - 1];
+
+//     if (!last || last.length === size) {
+//       chunked.push([element]);
+//     } else {
+//       last.push(element);
+//     }
+//   }
+
+//   return chunked;
+// }
 
 module.exports = chunk;
